79f5f8e6e8eb0f72594cdc08a276f94b7109ee2c
Tidbits JS <tidbitsjs@gmail.com>
2021-04-05 19:17:36 +0530

    delete userData

--- server.js
+++ server.js
@@ -100,7 +100,26 @@ app
       }
     );
   })
+
   // DELETE
   .delete((req, res) => {
     // User.findByIdAndDelete()
+    User.findByIdAndDelete(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err,
+        });
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: "Not Found",
+        });
+      } else {
+        res.json({
+          success: true,
+          data: data,
+        });
+      }
+    });
   });
